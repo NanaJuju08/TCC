7a1969a56a4af9762403795acd053749fe67d4d9
MirelaPedro <mirelapedrotereza@gmail.com>
2025-08-07 17:22:51 -0300

    add e ler do firestore

--- Backend/conect.py
+++ Backend/conect.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 
-credential = credentials.Certificate("C:\Users\mirela.30417\Documents\Chave-de-Conta/bylimas-osgabis-firebase-adminsdk-fbsvc-e1e38745eb.json")
+credential = credentials.Certificate("Chave-de-Conta/bylimas-osgabis-firebase-adminsdk-fbsvc-e1e38745eb.json")
 app = firebase_admin.initialize_app(credential)
 
 db = firestore.client()
