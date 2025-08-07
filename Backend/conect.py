24d8dad9720ac5d58c0e76527b422bc65d525428
MirelaPedro <mirelapedrotereza@gmail.com>
2025-08-07 16:05:57 -0300

    test

--- Backend/conect.py
+++ Backend/conect.py
@@ -2,9 +2,9 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 
-credential = credentials.Certificate("")
+credential = credentials.Certificate("C:\Users\mirela.30417\Documents\Chave-de-Conta/bylimas-osgabis-firebase-adminsdk-fbsvc-e1e38745eb.json")
 app = firebase_admin.initialize_app(credential)
 
 db = firestore.client()
 
-
+print("Banco conectado!")
